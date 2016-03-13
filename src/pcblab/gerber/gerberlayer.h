4cbd711b2cd5a463d00320c5961b8621d140ccf6
Vivien Henry <Vivien Henry>
2016-03-13 22:03:50 +0100

    Adding missing param to gerberlayer

--- src/pcblab/gerber/gerberlayer.h
+++ src/pcblab/gerber/gerberlayer.h
@@ -4,8 +4,12 @@
 #include <stdint.h>
 #include <vector>
 #include <string.h>
+
 #include "common.h"
 #include "graphicstate.h"
+#include "syntaxparser.h"
+#include "graphicobject.h"
+#include "aperture.h"
 
 using namespace std;
 
@@ -16,6 +20,12 @@ class GerberLayer {
 
         /// GerberLevel is a container holding all the graphic object with the same polarity
         class GerberLevel{
+            public:
+                GerberLevel(GraphicState::LevelPolarity inPolarity) mPolarity(inPolarity) {}
+
+                /// Adds an existing object to the level.
+                void addObject(GraphicObject *inObject) { mObject.push(inObject); }
+
             private:
                 GraphicState::eLevelPolarity mPolarity;
                 vector <GraphicObject *> mObjects;
@@ -32,10 +42,24 @@ class GerberLayer {
         bool open(string &inFileName);
 
     private:
+        /// Name of the Layer
+        string mName;
+
+        /// Store the current graphic state of the parsing.
+        GraphicState mState;
+
+        /// Store the syntax parser for this file.
+        SyntaxParser mParser;
 
         /// Handles the different GraphicObjects in a GerberLevel container.
         /// the 0 level is the bottom one, it can be overriden by upper layers.
         vector<GerberLevel> mLevels;
+
+        /// Aperture Dictionary.
+        vector<Aperture> mApertures;
+
+        /// Apertures Template Dict
+        vector<ApertureTemplate *> mApertureTemplates;
 };
 
 
