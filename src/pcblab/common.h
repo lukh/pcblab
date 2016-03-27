97b708d0f6864fc2dcaa682464a205d52e417fa2
Vivien HENRY <vivien.henry@outlook.fr>
2016-03-27 18:27:44 +0200

    Adding omitted coord concept

--- src/pcblab/common.h
+++ src/pcblab/common.h
@@ -30,12 +30,30 @@ typedef map<eExtension, string> PcbFiles;
 class Point{
 
   public:
-    Point(): mX(0.0), mY(0.0) {}
-    Point(double inX, double inY): mX(inX), mY(inY) {}
+    Point(): mX(0.0), mY(0.0), mIsXOmitted(false), mIsYOmitted(false) {}
+    Point(double inX, double inY): mX(inX), mY(inY), mIsXOmitted(false), mIsYOmitted(false) {}
 
+
+    /// updates the point regarding Is_Omitted, allowing to keep the previous value of one of the coordinates
+    void updateCoordinates(Point &inPoint){
+        //here, the point is modal (if one coordinate is ommitted, it uses the last one
+        if(! inPoint.mIsXOmitted) {
+            mX = inPoint.mX;
+        }
+
+        if(! inPoint.mIsYOmitted) {
+            mY = inPoint.mY;
+        }
+
+        mIsXOmitted=false;
+        mIsYOmitted=false;
+    }
 
     double mX;
     double mY;
+
+    bool mIsXOmitted;
+    bool mIsYOmitted;
 
 };
 
