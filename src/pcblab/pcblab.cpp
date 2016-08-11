55159725a16ab334bd5356c886bc8385517cfef4
Vivien Henry <vivien.henry@outlook.fr>
2016-08-11 13:30:30 +0200

    arch def of PcbLab

--- src/pcblab/pcblab.cpp
+++ src/pcblab/pcblab.cpp
@@ -4,3 +4,28 @@ PcbLab::PcbLab()
 {
 
 }
+
+void PcbLab::openFolder(string &inFolderName)
+{
+
+}
+
+void PcbLab::openGerberLayer(string &inName, string &inFilename, uint8_t inPosition)
+{
+    mGerber.openGerberLayer(inName, inFilename, inPosition);
+}
+
+void PcbLab::openPnPLayer(string &inFilename)
+{
+
+}
+
+void PcbLab::openNetlistLayer(string &inFilename)
+{
+
+}
+
+void PcbLab::closeFolder()
+{
+
+}
--- src/pcblab/pcblab.h
+++ src/pcblab/pcblab.h
@@ -1,7 +1,13 @@
 #ifndef PCBLAB_H
 #define PCBLAB_H
 
+#include <string>
+#include <iostream>
+#include <fstream>
+
 #include "gerber/gerberhandler.h"
+
+using namespace std;
 
 /// A manager for a full set of pcba object (gerber, nets, components)
 /// Main object manipulated by the UI
@@ -11,8 +17,22 @@ class PcbLab
         PcbLab();
 
 
-        //getters
-        GerberHandler &getGerberHandler() { return mGerberHandler; }
+        /// Opens a folder containing all CAD files (gerbers, pnp, netlists, etc)
+        void openFolder(string &inFolderName);
+
+
+        /// Opens a specific layer
+        void openGerberLayer(string &inName, string &inFilename, uint8_t inPosition);
+
+        /// Opens a PickAndPlace layer
+        void openPnPLayer(string &inFilename);
+
+        /// Open a netlist
+        void openNetlistLayer(string &inFilename);
+
+
+        /// Closes the folder
+        void closeFolder();
 
 
     private:
