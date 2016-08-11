1c6b96aee38718c2aeaae70d50ac0a5f4db7bdd9
Vivien Henry <vivien.henry@outlook.fr>
2016-08-11 13:29:50 +0200

    processorwrapper update

--- src/gui/processorwrapper.cpp
+++ src/gui/processorwrapper.cpp
@@ -1,6 +1,2 @@
 #include "processorwrapper.h"
 
-ProcessorWrapper::ProcessorWrapper()
-{
-
-}
--- src/gui/processorwrapper.h
+++ src/gui/processorwrapper.h
@@ -1,11 +1,21 @@
 #ifndef PROCESSORWRAPPER_H
 #define PROCESSORWRAPPER_H
 
+#include <QThread>
+#include "pcblab/viewprocessor/viewprocessor.h"
 
 class ProcessorWrapper
 {
-public:
-    ProcessorWrapper();
+    public:
+        ProcessorWrapper(ViewProcessor &inProcessor): mProcessor(inProcessor) {}
+
+    protected:
+        virtual void run(){
+            mProcessor.update();
+        }
+
+    private:
+        ViewProcessor &mProcessor;
 };
 
 #endif // PROCESSORWRAPPER_H
